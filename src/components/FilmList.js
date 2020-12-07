28851d12b6ea545fc9ce0eabcf41711e55fc3a57
AvianLearning <iaradcliffe@hotmail.com>
2020-12-07 16:08:04 +0000

    Added dummy FilmList component and can see in browser

---
+++ src/components/FilmList.js
@@ -0,0 +1,10 @@
+const FilmList = () => {
+
+    return(
+        <>
+        <h2>This will be the film list</h2>
+        </>
+    )
+};
+
+export default FilmList;
--- src/containers/Display.js
+++ src/containers/Display.js
@@ -1,7 +1,12 @@
+import FilmList from '../components/FilmList'
+
 const Display = () => {
 
     return (
+        <>
         <h2>This is the display container</h2>
+        <FilmList />
+        </>
     );
 };
 
