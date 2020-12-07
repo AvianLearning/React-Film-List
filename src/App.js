5e5f9273e9f2c362c96bad562777975aa6079a12
AvianLearning <iaradcliffe@hotmail.com>
2020-12-07 16:04:37 +0000

    Added dummy Display container and rendered in browser

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Display from './containers/Display'
 
 function App() {
   return (
     <>
       <h1>Upcoming UK Film releases</h1>
+      <Display />
     </>
     
   );
---
+++ src/containers/Display.js
@@ -0,0 +1,8 @@
+const Display = () => {
+
+    return (
+        <h2>This is the display container</h2>
+    );
+};
+
+export default Display;
